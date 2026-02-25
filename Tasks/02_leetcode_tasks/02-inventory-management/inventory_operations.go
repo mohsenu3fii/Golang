23d58d34e1c90c52e814c99f03d6de271e1ccd4b
Amir Salkhori <50391671+amirsalkhori@users.noreply.github.com>
2026-02-25 21:31:40 +0330

    Update inventory_operations.go

--- Tasks/02_leetcode_tasks/02-inventory-management/inventory_operations.go
+++ Tasks/02_leetcode_tasks/02-inventory-management/inventory_operations.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 
+//Why do you use *Product for the value of your inventory?
 func AddProduct(inventory map[int]*Product, product *Product) error {
 
+	//You can choose a better name like data, exist
 	if existing_data, is_exist := inventory[product.ID]; is_exist {
 
 		// If product already exists, increase the quantity
@@ -12,14 +14,17 @@ func AddProduct(inventory map[int]*Product, product *Product) error {
 		if existing_data.Price != product.Price {
 			existing_data.Price = product.Price
 		}
-
-	} else {
+// You can return nil this line
+	} else { // Remove this else
 		// If product is new, add it
 		inventory[product.ID] = product
 	}
+
+	//Why do you use error when a product added?
 	return fmt.Errorf("product added")
 }
 
+//Why do you use pointer for product?
 func (p ProductP) UpdateProduct(inventory map[int]*Product, productID int) error {
 	if existingProduct, ok := inventory[productID]; ok {
 		// Only update non-nil fields
@@ -37,6 +42,8 @@ func (p ProductP) UpdateProduct(inventory map[int]*Product, productID int) error
 		}
 		return nil
 	}
+
+	//Instead of use "product not found" as a string, use const
 	return fmt.Errorf("product not found")
 }
 
@@ -44,18 +51,24 @@ func RemoveProduct(inventory map[int]Product, productID int) error {
 	// If product doesn't exist, return an error
 	if _, is_exist := inventory[productID]; is_exist {
 		delete(inventory, productID)
-	} else {
+		// return nil
+	} else {//Remove this else
 		return fmt.Errorf("product not fount")
 	}
+
+	//you must return nil and write a log as an info not return error
 	return fmt.Errorf("product deleted")
 }
 
+//You can use a single response and it be int instead of (int, bool)
 func CheckStock(inventory map[int]*Product, productID int) (int, bool) {
 	// First value: product quantity
 	// Second value: whether product exists or not
 	if existing_data, is_exist := inventory[productID]; is_exist {
+
+		//Just return the quantity
 		return existing_data.Quantity, is_exist
-	} else {
+	} else {// remove this else
 		return 0, false
 	}
 }
